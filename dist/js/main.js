eabe26bbb6d99d69096ffa7f3da64d99ec8976d9
Edward Tanguay (windows) <edward@tanguay.info>
2025-07-18 17:30:58 +0200

    Add webpack.config.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -26,7 +26,7 @@ eval("{var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!\n * j
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! jquery */ \"./node_modules/jquery/dist/jquery.js\");\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\r\njquery__WEBPACK_IMPORTED_MODULE_0___default()('#title').html('Hello, Webpack!');\r\njquery__WEBPACK_IMPORTED_MODULE_0___default()('.message').html('This is a simple Webpack setup, created with npx, ver 3, test: subdir');\n\n//# sourceURL=webpack://webpack-basics/./src/index.js?\n}");
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! jquery */ \"./node_modules/jquery/dist/jquery.js\");\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\r\njquery__WEBPACK_IMPORTED_MODULE_0___default()('#title').html('Hello, Webpack!');\r\njquery__WEBPACK_IMPORTED_MODULE_0___default()('.message').html('This is a simple Webpack setup, created with npx, ver 4, test: subdir, config');\n\n//# sourceURL=webpack://webpack-basics/./src/index.js?\n}");
 
 /***/ })
 
