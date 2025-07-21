dec5db3a0d121a3048e04829d9e45bf9622fd41b
Edward Tanguay (windows) <edward@tanguay.info>
2025-07-21 08:38:41 +0200

    FEATURE: Add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# webpack-basics
+
+This is a basic HTML/CSS/JS site set up with Webpack 5.
+
+# setup
+
+- `npm i`
+- right-click on dist/index.html, Open with Live Server
+
+# make modifications
+
+- edit **src/index.js**
+- `npx webpack`
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import $ from 'jquery';
 
 $('#title').html('Hello, Webpack!');
-$('.message').html('This is a simple Webpack setup, created with npx, ver 4, test: subdir, config, dev');
+$('.message').html('This is a simple Webpack setup, created with npx, ver 5, test: subdir, config, dev');
