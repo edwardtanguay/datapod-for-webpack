0f91c52acc46c164042d2b7ab16ab2f46944b6b5
Edward Tanguay (windows) <edward@tanguay.info>
2025-07-21 12:02:47 +0200

    DEVFEATURE: Add Babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,42 @@ const path = require('path');
 module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'js/main.js'
+		filename: 'js/main.js',
 	},
 	mode: 'development',
 	watchOptions: {
-		ignored: /node_modules/
+		ignored: /node_modules/,
 	},
 	module: {
 		rules: [
 			{
 				test: /\.css|scss$/,
-				use: ['style-loader', 'css-loader',
+				use: [
+					'style-loader',
+					'css-loader',
 					{
 						loader: 'postcss-loader',
 						options: {
 							postcssOptions: {
-								plugins: [
-									require('autoprefixer')
-								]
-							}
-						}
-					 }, 'sass-loader'],
-			}
-		]
-	}
+								plugins: [require('autoprefixer')],
+							},
+						},
+					},
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env'],
+						},
+					},
+				],
+			},
+		],
+	},
 };
