47721c0de88146bf4e86f3af839298c1a33e59df
Edward Tanguay (windows) <edward@tanguay.info>
2025-07-21 10:44:30 +0200

    DEVFEATURE: Install bootstrap basics

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
+import bootstrap from 'bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 $('#title').html('Hello, Webpack!');
 $('.message').html('This is a simple Webpack setup, created with npx, ver 13, test: subdir, config, dev');
